e6ffe0aaa3702ba785b68ae3b09b44edb4b47b14
Mr.Sharvin <93881528+SharvinKumarArumugam@users.noreply.github.com>
2022-04-03 10:56:39 +0800

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -10,7 +10,14 @@
 //main function section 
 int main(){
     //write here your program
+    float currencyWorld[5];//declaration array
+    currencyWorld[0]=4.50;//initialization array
+    currencyWorld[1]= 5.55;
+    currencyWorld[2]=3.65;
+    currencyWorld[3]=1.20;
+    currencyWorld[4]=6.56;
 
+    printf(" %.2f",currencyWorld[3]);
 
     return 0;
 }
